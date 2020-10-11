d7a54ecdefe50ad5da7136d9091e7c6429bbebc4
André de Sousa Araújo <dedeco@gmail.com>
2020-10-11 13:48:04 -0300

    Add new test pass, while keeping all the old tests passing.

--- test/math/addFractions.test.js
+++ test/math/addFractions.test.js
@@ -5,5 +5,9 @@ describe('Add fractions tests', function () {
     it('A zero plus zero test', function () {
         expect(addFraction(0, 0)).toBe(0);
     })
+
+    it('A non zero plus zero test', function () {
+        expect(addFraction(0, 3)).toBe(3);
+    })
 })
 
