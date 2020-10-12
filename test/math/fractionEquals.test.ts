f585f425876793995efbbff8e3a9f1bd06976f51
André de Sousa Araújo <dedeco@gmail.com>
2020-10-12 00:14:29 -0300

    Equals is working fine

--- test/math/fractionEquals.test.ts
+++ test/math/fractionEquals.test.ts
@@ -5,4 +5,12 @@ describe('Fractions equals tests', function () {
         expect(new Fraction(3,5)).toEqual((new Fraction(3,5)));
     })
 
+    it('Diferent denominators', function () {
+        expect(new Fraction(3,4)).not.toEqual((new Fraction(3,7)));
+    })
+
+    it('Whole number not equals to different whole number', function () {
+        expect(new Fraction(6)).not.toEqual((new Fraction(5)));
+    })
+
 })
